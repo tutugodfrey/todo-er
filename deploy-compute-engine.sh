225f050b6818b98daa8c96e932e9030f15af49f3
tutugodfrey <godfrey_tutu@yahoo.com>
2020-09-22 02:31:08 +0000

    - Add API port to .env.example - Add \ to --address line in deployment script

--- deploy-compute-engine.sh
+++ deploy-compute-engine.sh
@@ -12,11 +12,11 @@ gcloud compute firewall-rules create default-allow-http-access \
 gcloud compute addresses create mc-server-ip --project=$project --region=us-central1 # create a static ip address (reserved)
 ipaddress=$(gcloud compute addresses describe mc-server-ip --region us-central1 | grep address: | sed 's/address: /'""/) # get the value of the ip address
 echo reserved ip address is $ipaddress
-sed -i -e 's/SERVERIP/'$ipaddress/
+sed -i -e 's/SERVERIP/'$ipaddress/ ./startup.sh
 gcloud compute instances create todoapp-vm \
 --zone us-central1-a \
 --tags http-server \
---address=$ipaddress
+--address=$ipaddress \
 --metadata-from-file startup-script=startup.sh
 
 # Get the vm external ip address
