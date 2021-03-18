ce49f2d3504c61edd4af6f9957322d616481a7a4
Godfrey Tutu <godfreytutu@Godfreys-MacBook-Pro.local>
2021-03-18 14:55:35 +0100

    Configure and provision datacenter on AWS with Terraform
    - Create VPC, private and public subnets
    - Create security group
    - Provision a nat gateway to enable servers in private subnet access internet
    - Provision servers ( 2 app server, lb server, storage server, jenkins server, jump server, db server)
    - Ensure app servers are reachable from the internet

---
+++ tf-aws/variables.tf
@@ -0,0 +1,35 @@
+variable "profile" {
+  default = "tgodfrey"
+}
+variable "region" {
+  default = "us-west-2"
+}
+variable "vpc_cidr_block" {}
+variable "public_subnet_a_cidr" {}
+variable "private_subnet_a_cidr" {}
+variable "public_subnet_b_cidr" {}
+variable "private_subnet_b_cidr" {}
+variable "ec2_instance_type" {}
+variable "ec2_keypair" {}
+variable "app_server_ami_id" {}
+variable "jenkins_server_ami_id" {}
+variable app_server_1_private_ip {}
+variable app_server_2_private_ip {}
+variable "lb_server_private_ip" {}
+variable "jenkins_server_private_ip" {}
+variable "jump_server_private_ip" {}
+variable "storage_server_private_ip" {}
+variable "db_server_private_ip" {}
+variable "jenkins_server_hostname" {}
+variable "storage_server_hostname" {}
+variable "jump_server_hostname" {}
+variable "app_server_1_hostname" {}
+variable "app_server_2_hostname" {}
+variable "lb_server_hostname" {}
+variable "db_server_hostname" {}
+variable "db_name" {}
+variable "db_user_name" {}
+variable "db_user_pass" {}
+variable "db_port" {
+  default = 5432
+}
