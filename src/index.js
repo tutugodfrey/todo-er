809d75385d4575f1daae6837f8a795cb5d939bef
tutugodfrey <godfrey_tutu@yahoo.com>
2021-06-22 20:24:28 +0100

    Prometheus
    - Configure Prometheus client for Node.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './routes/index'
+import router from './routes/index';
+import promClient from 'prom-client';
 import cors from 'cors';
 
 const app = express();
@@ -10,6 +11,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use('/api', router)
+
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+app.get('/metrics', (req, res) => {
+  const Registry = promClient.Registry;
+  const registry = new Registry();
+  const metrics = collectDefaultMetrics({ registry })
+  res.json({ message: metrics })
+});
 
 // important to keep catch-all route last
 app.get('/*', (req, res) => {
